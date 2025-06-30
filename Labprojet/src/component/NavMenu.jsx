148fa3207352715aaac8d61672f407ccc6331a01
Mihindou7 <153740250+Mihindou7@users.noreply.github.com>
2025-06-30 15:12:23 +0200

    Modification du composant NavMenu

---
+++ Labprojet/src/component/NavMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavMenu.css';
+
+export default function NavMenu() {
+  return (
+    <nav className="nav-menu">
+      <ul>
+        <li>TOURNÉE</li>
+        <li>T-SHIRT</li>
+        <li>SWEAT</li>
+        <li>VINYLE</li>
+      </ul>
+    </nav>
+  );
+}
